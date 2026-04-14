2ce60a3653abe39a0cd9332d1d6fa00a3af8e534
c0a25242 <c0a2524217@edu.teu.ac.jp>
2026-04-14 16:20:23 +0900

    練習１０

--- pygame_image.py
+++ pygame_image.py
@@ -13,16 +13,27 @@ def main():
     bg2_img = pg.transform.flip(bg_img, True, False)#練習8
     kk_img = pg.image.load("fig/3.png")#練習3
     kk_img = pg.transform.flip(kk_img, True, False) 
+    kk_rct = kk_img.get_rect() #練習10rectの取得
+    kk_rct.center = 300, 200 #練習10初期座標の設定
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
+        key_list = pg.key.get_pressed() #練習10すべてのキーの押下状態の取得
+        if key_list[pg.K_UP]:
+            kk_rct.move_ip(0, -1)
+        if key_list[pg.K_DOWN]:
+            kk_rct.move_ip(0, 1)
+        if key_list[pg.K_LEFT]:
+            kk_rct.move_ip(-1, 0)
+        if key_list[pg.K_RIGHT]:
+            kk_rct.move_ip(1, 0)
         x = tmr%3200 #練習5 #練習9
         screen.blit(bg_img, [-x, 0])#練習2
         screen.blit(bg2_img, [-x+1600, 0])#練習7
         screen.blit(bg_img, [-x+3200, 0])#練習8
-        screen.blit(kk_img, [300, 200])#練習4
+        screen.blit(kk_img, kk_rct)#練習4 #練習10
         pg.display.update()
         tmr += 1        
         clock.tick(200) #練習6
